7f6e77a59c9d5aec47788efa31b1f96dfe629c8c
rafaelascolaro <142499158+rafaelascolaro@users.noreply.github.com>
2023-10-17 12:43:08 +0000

    aula

---
+++ js/myscript.js
@@ -0,0 +1,10 @@
+//aqui vai javascript
+function trocarFrase() {
+    var fraseAtual = document.getElementById("frase");
+    fraseAtual.innerHTML = "Funcionou. a frase mundou!";
+    fraseAtual.style.color = "red";
+    fraseAtual.style.fontSize = "25px";
+    fraseAtual.style.backgroundColor = "yellow";
+    document.body.style.backgroundColor = "blue";
+    elemento.innerHTML = "<img src=\'img/a.jpeg\' />"
+}
